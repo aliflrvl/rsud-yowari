e60c0fb6f654972e51d03ca4a4ae04c2a61cc4fe
aliflrvl <alifprtm27041991@gmail.com>
2026-01-30 14:04:03 +0900

    Add files via upload

---
+++ admin/modul/mod_tempattidur.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 24px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style14 {font-size: 14px}
+.style22 {color: #FFFF00}
+.style24 {
+	font-size: 14px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style25 {font-family: Arial, Helvetica, sans-serif}
+.style26 {font-family: Arial, Helvetica, sans-serif; font-size: 14px; }
+-->
+</style>
+</head>
+
+<p class="style1"> INFORMASI TEMPAT TIDUR RSUD YOWARI SENTANI</p>
+<table cellpadding="0" cellspacing="0" bordercolor="#FFFFFF">
+  <tr>
+    <td width="31" height="33"></td>
+    <td width="204" bgcolor="#0000FF" class="xl71 style22"><div align="center" class="style24">NAMA RUANG </div></td>
+    <td width="140" bgcolor="#0000FF" class="xl71 style22"><div align="center" class="style14 style25"><strong>KELAS </strong></div></td>
+    <td width="160" bgcolor="#0000FF" class="xl71 style22"><div align="center" class="style24">KAPASITAS </div></td>
+  </tr>
+  <tr>
+    <td height="23"></td>
+    <td height="23" class="xl65 style25 style14">YP KARAFIR </td>
+    <td height="23" class="xl66"><div align="center" class="style26">VIP </div></td>
+    <td height="23" class="xl66"><div align="center" class="style26">1 </div></td>
+  </tr>
+  <tr>
+    <td height="25" class="xl65"></td>
+    <td height="25" bgcolor="#FFFF00" class="xl67 style25 style14">RUNAG BUPATI </td>
+    <td height="25" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">KELAS I </div></td>
+    <td height="25" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">12 </div></td>
+  </tr>
+  <tr>
+    <td height="23" class="xl65"></td>
+    <td height="23" class="xl69 style25 style14">RUANG CENDERAWASIH </td>
+    <td height="23" class="xl70"><div align="center" class="style26">KELAS I </div></td>
+    <td height="23" class="xl70"><div align="center" class="style26">10 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG NURI </td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">KELAS II </div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">8 </div></td>
+  </tr>
+  <tr>
+    <td height="25" class="xl65"></td>
+    <td height="25" class="xl65 style25 style14">RUANG KANAK - KANAK </td>
+    <td height="25" class="xl66"><div align="center" class="style26">KELAS II </div></td>
+    <td height="25" class="xl66"><div align="center" class="style26">4 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG BEDAH WANITA </td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">KELAS III </div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">8 </div></td>
+  </tr>
+  <tr>
+    <td height="23" class="xl65"></td>
+    <td height="23" class="xl65 style25 style14">RUANG BEDAH PRIA </td>
+    <td height="23" class="xl66"><div align="center" class="style26">KELAS III </div></td>
+    <td height="23" class="xl66"><div align="center" class="style26">8 </div></td>
+  </tr>
+  <tr>
+    <td height="23" class="xl65"></td>
+    <td height="23" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG INTERNA WANITA </td>
+    <td height="23" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">KELAS III </div></td>
+    <td height="23" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">14 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" class="xl65 style25 style14">RUANG INTERNA PRIA </td>
+    <td height="24" class="xl66"><div align="center" class="style26">KELAS III </div></td>
+    <td height="24" class="xl66"><div align="center" class="style26">14 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG PERINA </td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">NICU / PICU </div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">23</div></td>
+  </tr>
+  <tr>
+    <td height="22" class="xl65"></td>
+    <td height="22" class="xl65 style25 style14">RUANG NIFAS </td>
+    <td height="22" class="xl66"><div align="center" class="style26">KELAS III </div></td>
+    <td height="22" class="xl66"><div align="center" class="style26">8 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG ICU</td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">ICU</div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">4</div></td>
+  </tr>
+  <tr>
+    <td height="22" class="xl65"></td>
+    <td height="25" class="xl65 style25 style14">RUANG KANAK - KANAK </td>
+    <td height="25" class="xl66"><div align="center" class="style26">KELAS II </div></td>
+    <td height="25" class="xl66"><div align="center" class="style26">4 </div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG ISOLASI COVID </td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">ISOLASI</div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">10</div></td>
+  </tr>
+  <tr>
+    <td height="22" class="xl65"></td>
+    <td height="25" class="xl65 style25 style14">RUANG NIFAS </td>
+    <td height="25" class="xl66"><div align="center" class="style26">KELAS II </div></td>
+    <td height="25" class="xl66"><div align="center" class="style26">14</div></td>
+  </tr>
+  <tr>
+    <td height="24" class="xl65"></td>
+    <td height="24" bgcolor="#FFFF00" class="xl67 style25 style14">RUANG VK </td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">KELAS III </div></td>
+    <td height="24" bgcolor="#FFFF00" class="xl68"><div align="center" class="style26">8</div></td>
+  </tr>
+  <tr>
+    <td height="22" class="xl65"></td>
+    <td height="25" class="xl65 style25 style14">RUANG PONEK</td>
+    <td height="25" class="xl66"><div align="center" class="style26">KELAS III</div></td>
+    <td height="25" class="xl66"><div align="center" class="style26">4</div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
---
+++ admin/modul/mod_video.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 24px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style14 {font-size: 14px}
+.style22 {color: #FFFF00}
+.style24 {
+	font-size: 14px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style25 {font-family: Arial, Helvetica, sans-serif}
+.style26 {font-family: Arial, Helvetica, sans-serif; font-size: 14px; }
+-->
+</style>
+</head>
+
+<p class="style1"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;VIDEO YOWARI</p>
+<table width="580" border="0">
+  <tr>
+    <th width="5" height="109" scope="col">&nbsp;</th>
+    <th width="180" scope="col"><img src="images/vid_profil.jpg" width="176" height="136"></th>
+    <th width="180" scope="col"><img src="images/vid_etika_batuk.jpg" width="176" height="136"></th>
+    <th width="180" scope="col"><img src="images/vid_6_langkah.jpg" width="176" height="136"></th>
+  </tr>
+  <tr>
+    <th height="21" scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th height="128" scope="row">&nbsp;</th>
+    <td><img src="images/vid_apd_level1.jpg" width="176" height="136"></td>
+    <td><img src="images/vid_apd_level2.jpg" width="176" height="136"></td>
+    <td><img src="images/vid_profil.jpg" width="176" height="136"></td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th height="143" scope="row">&nbsp;</th>
+    <td><a href="images/pcr_.jpg"><img src="images/vid_vaksin.jpg" width="176" height="136" border="0"></a></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <th scope="row">&nbsp;</th>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+
